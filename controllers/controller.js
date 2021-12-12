2101aa040a68740a29c8bbccdbc026943b72a5e7
magalhaes <joao@git.com>
2021-12-12 22:35:02 +0000

    End

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 
 const db = require("../models/nedb"); // Define o MODEL que vamos usar
 const bcrypt = require("bcrypt");
-const axios = require('axios')
-const cheerio = require('cheerio')  
 const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res) {
@@ -27,7 +25,7 @@ async function enviaEmail(recipients, confirmationToken) {
   // Gera uma conta do serviço SMTP de email do domínio ethereal.email
   // Somente necessário na fase de testes e se não tiver uma conta real para utilizar
   let testAccount = await nodemailer.createTestAccount();
-
+  
   // Cria um objeto transporter reutilizável que é um transporter SMTP
   let transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
@@ -38,7 +36,7 @@ async function enviaEmail(recipients, confirmationToken) {
       pass: testAccount.pass, // senha do utilizador ethereal
     },
   });
-
+  
   // envia o email usando o objeto de transporte definido
   let info = await transporter.sendMail({
     from: '"Fred Foo 👻" <foo@example.com>', // endereço do originador
@@ -47,79 +45,79 @@ async function enviaEmail(recipients, confirmationToken) {
     text: "Clique aqui para ativar sua conta: " + confirmationToken, // corpo do email
     html: "<b>Clique aqui para ativar sua conta: " + confirmationToken + "</b>", // corpo do email em html
   });
-
+  
   console.log("Mensagem enviada: %s", info.messageId);
   // Mensagem enviada: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
+  
   // A pré-visualização só estará disponível se usar uma conta Ethereal para envio
   console.log(
     "URL para visualização prévia: %s",
     nodemailer.getTestMessageUrl(info)
-  );
-  // URL para visualização prévia: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-exports.verificaUtilizador = async (req, res) => {
-  const confirmationCode = req.params.confirmationCode;
-  db.crUd_ativar(confirmationCode);
-  const resposta = { message: "O utilizador está ativo!" };
-  console.log(resposta);
-  return res.send(resposta);
-};
-
-// REGISTAR - cria um novo utilizador
-exports.registar = async (req, res) => {
-  console.log("Registar novo utilizador");
-  if (!req.body) {
-    return res.status(400).send({
-      message: "O conteúdo não pode ser vazio!",
-    });
+    );
+    // URL para visualização prévia: https://ethereal.email/message/WaQKMgKddxQDoou...
   }
-  const salt = await bcrypt.genSalt();
-  const hashPassword = await bcrypt.hash(req.body.password, salt);
-  const email = req.body.email;
-  const password = hashPassword;
-  const confirmationToken = jwt.sign(
-    req.body.email,
-    process.env.ACCESS_TOKEN_SECRET
-  );
-  const confirmURL = `http://localhost:${process.env.PORT}/api/auth/confirm/${confirmationToken}`
-  db.Crud_registar(email, password, confirmationToken) // C: Create
-    .then((dados) => {
-      enviaEmail(email, confirmURL).catch(console.error);
-      res.status(201).send({
-        message:
-          "Utilizador criado com sucesso, confira sua caixa de correio para ativar!",
+  
+  exports.verificaUtilizador = async (req, res) => {
+    const confirmationCode = req.params.confirmationCode;
+    db.crUd_ativar(confirmationCode);
+    const resposta = { message: "O utilizador está ativo!" };
+    console.log(resposta);
+    return res.send(resposta);
+  };
+  
+  // REGISTAR - cria um novo utilizador
+  exports.registar = async (req, res) => {
+    console.log("Registar novo utilizador");
+    if (!req.body) {
+      return res.status(400).send({
+        message: "O conteúdo não pode ser vazio!",
       });
-      console.log("Controller - utilizador registado: ");
-      console.log(JSON.stringify(dados)); // para debug
-    })
-    .catch((response) => {
-      console.log("controller - registar:");
-      console.log(response);
-      return res.status(400).send(response);
-    });
-};
-
-// LOGIN - autentica um utilizador
-exports.login = async (req, res) => {
-  console.log("Autenticação de um utilizador");
-  if (!req.body) {
-    return res.status(400).send({
-      message: "O conteúdo não pode ser vazio!",
-    });
-  }
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(req.body.password, salt);
-  const email = req.body.email;
-  const password = hashPassword;
-  db.cRud_login(email) //
-    .then(async (dados) => {
-      if (await bcrypt.compare(req.body.password, dados.password)) {
-        const user = { name: email };
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-        res.json({ accessToken: accessToken }); // aqui temos de enviar a token de autorização
-        console.log("Resposta da consulta à base de dados: ");
+    }
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const email = req.body.email;
+    const password = hashPassword;
+    const confirmationToken = jwt.sign(
+      req.body.email,
+      process.env.ACCESS_TOKEN_SECRET
+      );
+      const confirmURL = `http://localhost:${process.env.PORT}/api/auth/confirm/${confirmationToken}`
+      db.Crud_registar(email, password, confirmationToken) // C: Create
+      .then((dados) => {
+        enviaEmail(email, confirmURL).catch(console.error);
+        res.status(201).send({
+          message:
+          "Utilizador criado com sucesso, confira sua caixa de correio para ativar!",
+        });
+        console.log("Controller - utilizador registado: ");
+        console.log(JSON.stringify(dados)); // para debug
+      })
+      .catch((response) => {
+        console.log("controller - registar:");
+        console.log(response);
+        return res.status(400).send(response);
+      });
+    };
+    
+    // LOGIN - autentica um utilizador
+    exports.login = async (req, res) => {
+      console.log("Autenticação de um utilizador");
+      if (!req.body) {
+        return res.status(400).send({
+          message: "O conteúdo não pode ser vazio!",
+        });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(req.body.password, salt);
+      const email = req.body.email;
+      const password = hashPassword;
+      db.cRud_login(email) //
+      .then(async (dados) => {
+        if (await bcrypt.compare(req.body.password, dados.password)) {
+          const user = { name: email };
+          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+          res.json({ accessToken: accessToken }); // aqui temos de enviar a token de autorização
+          console.log("Resposta da consulta à base de dados: ");
         console.log(JSON.stringify(dados)); // para debug
       } else {
         console.log("Password incorreta");
@@ -131,17 +129,19 @@ exports.login = async (req, res) => {
       console.log(response);
       return res.status(400).send(response);
     });
-};
-
-// const express = require('express')
-// const app = express()
-// const { response } = require('express')
-// const { last } = require('cheerio/lib/api/traversing')
-
-const websites = [
+  };
+  
+  // const express = require('express')
+  const axios = require('axios')
+  const cheerio = require('cheerio')  
+  // const app = express()
+  // const { response } = require('express')
+  // const { last } = require('cheerio/lib/api/traversing')
+  
+  const websites = [
     {
-        name: 'BackMarket',
-        address: 'https://www.backmarket.pt/apple-recondicionados.html',
+      name: 'BackMarket',
+      address: 'https://www.backmarket.pt/apple-recondicionados.html',
         base: 'https://www.backmarket.pt'
     },
     {
@@ -226,16 +226,7 @@ exports.FirstPage = (req, res) => {
 }
 
 exports.findAll = (req, res) => {
-  authenticateToken(req, res);
-  if (req.email != null) {
-    console.log(`FindAll - user: ${req.email.name}`);
-    res.json(products)
-  }
-    // console.log("token: " + authenticateToken)
-    // if (req.email != null) {
-    // // utilizador autenticado
-    //   console.log(`FindAll - user: ${req.email.name}`);
-    // }
+  res.json(products)
 }
 
 exports.findOne = async (req, res) => {
@@ -261,17 +252,15 @@ exports.findOne = async (req, res) => {
                         const specifications = $('span:Contains("GB")', this).text().trim()
                         const Warranty = $('span:Contains("Garantia")', this).text().trim()
                         const price = $('span:Contains("€")', this).first().text().trim()
-                        //var Myimage = new Image()
-                        // image.src = $(this)
-                        //console.log("image: "+ Myimage)
+                        const image = $('img[src]' ,this)
                         specificproducts.push({
                             title,
                             specifications,
                             Warranty,
                             price,
                             url: websiteBase + url,
-                            source: websiteId
-                            //image
+                            source: websiteId,
+                            image
                         })
                     })
                 })
